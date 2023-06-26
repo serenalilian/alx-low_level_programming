b07f05df4814c07cfe2245110651297a47056601
serenalilian <serenalilian99@gmail.com>
2023-06-26 05:58:29 -0700

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _puts - prints a string, followed by a new line, to stdout
+  * @str: _puts function parameter
+  */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
